19696a8dbe2949e77c236c17da8e61a11171291a
Jiro Kugiya <j.kugiya@gmail.com>
2019-04-22 17:41:23 +0900

    Uses simple implementation(refered https://github.com/plokhotnyuk/jsoniter-scala/blob/3667262680d6ee527058dbd8cbebdacdec305c97/jsoniter-scala-macros/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/JsonCodecMaker.scala#L114-L151)

--- src/main/scala/spray/json/SnakeCaseJsonSupport.scala
+++ src/main/scala/spray/json/SnakeCaseJsonSupport.scala
@@ -8,45 +8,37 @@ import scala.collection.mutable
   */
 trait SnakeCaseJsonSupport extends DefaultJsonProtocol {
 
-  override protected def extractFieldNames(classTag: ClassTag[_]): Array[String] = {
-    def snakify(name: String) = {
-      val chars = name.toCharArray
-      val len = name.length
-      val sb = new mutable.StringBuilder(len + len / 5)
+  override protected def extractFieldNames(classTag: ClassTag[_]): Array[String] =
+    super.extractFieldNames(classTag).map(enforceSnakeCase)
 
-      def isAlphabetic(char: Char): Boolean = (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
-
-      def go(i: Int, rest: Int, processedUpper: Boolean, processedAlphaNumeric: Boolean): String =
-        if (rest == 0) {
-          sb.toString()
-        } else if (rest > 1 && chars(i).isUpper && chars(i + 1).isLower) {
-          if (processedAlphaNumeric) {
-            sb.append('_')
-          }
-          sb.append(chars(i).toLower).append(chars(i + 1))
-          go(i + 2, rest - 2, false, true)
-        } else if (chars(i).isUpper) {
-          if (!processedUpper && processedAlphaNumeric) {
-            sb.append('_')
-          }
-          sb.append(chars(i).toLower)
-          go(i + 1, rest - 1, true, chars(i).isDigit || isAlphabetic(chars(i)))
-        } else if (!isAlphabetic(chars(i))) {
-          sb.append(chars(i))
-          go(i + 1, rest - 1, false, chars(i).isDigit)
-        } else {
-          sb.append(chars(i).toLower)
-          go(i + 1, rest - 1, chars(i).isUpper, chars(i).isDigit || isAlphabetic(chars(i)))
-        }
-
-      go(0, len, true, false)
+  /**
+    * Mapping function for field or class names that should be in snake_case format.
+    *
+    * @param s the name to transform
+    * @return a transformed name or the same name if no transformation is required
+    */
+  private[this] def enforceSnakeCase(s: String): String = {
+    val len = s.length
+    val sb = new StringBuilder(len << 1)
+    var i = 0
+    var isPrecedingLowerCased = false
+    while (i < len) isPrecedingLowerCased = {
+      val ch = s.charAt(i)
+      i += 1
+      if (ch == '_') {
+        sb.append('_')
+        false
+      } else if (Character.isLowerCase(ch)) {
+        sb.append(ch)
+        true
+      } else {
+        if (isPrecedingLowerCased || i > 1 && i < len && Character.isLowerCase(s.charAt(i))) sb.append('_')
+        sb.append(Character.toLowerCase(ch))
+        false
+      }
     }
-
-    super.extractFieldNames(classTag).map {
-      snakify(_)
-    }
+    sb.toString
   }
-
 }
 
 object SnakeCaseJsonSupport extends SnakeCaseJsonSupport
